1c897c2c18600dcbe322d2f50fecef79fc9a6788
Pushpal Roy <pushpalroy2007@gmail.com>
2023-02-11 04:38:52 +0530

    feat: add ferry [contains errors]

--- data/lib/sources/network/graphql/graphql_service.dart
+++ data/lib/sources/network/graphql/graphql_service.dart
@@ -1,50 +1,48 @@
 import 'dart:developer';
 import 'package:data/sources/network/exceptions/api_exception.dart';
+import 'package:data/sources/network/graphql/query/__generated__/character_card.req.gql.dart';
 import 'package:domain/entities/api_response.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:ferry/ferry.dart';
+import 'package:ferry/typed_links.dart';
+import 'package:gql_http_link/gql_http_link.dart';
+import 'package:ferry_hive_store/ferry_hive_store.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:injectable/injectable.dart';
 
 @singleton
 class GraphQLService {
-  late final GraphQLClient _client;
+  late final Future<Client> _client;
 
   GraphQLService() {
-    HttpLink link =
-        HttpLink("https://rickandmortyapi.com/graphql", defaultHeaders: {});
+    _client = initClient();
+  }
 
-    _client =
-        GraphQLClient(link: link, cache: GraphQLCache(store: InMemoryStore()));
+  Future<Client> initClient() async {
+    await Hive.initFlutter();
+    final box = await Hive.openBox<Map<String, dynamic>>("graphql");
+    await box.clear();
+    final store = HiveStore(box);
+    final cache = Cache(store: store);
+    final link = HttpLink("https://rickandmortyapi.com/graphql");
+    final client = Client(
+      link: link,
+      cache: cache,
+    );
+    return client;
   }
 
   Future<ApiResponse> performQuery(String query,
       {required Map<String, dynamic> variables}) async {
     try {
-      QueryOptions options = QueryOptions(
-        document: gql(query),
-        variables: variables,
-      );
-
-      final result = await _client.query(options);
-
-      if (result.hasException) {
-        return Failure(error: APIException(result.exception.toString(), 0, ''));
-      } else {
-        return Success(data: result.data);
-      }
+      final client = await _client;
+      final charactersReq = GCharacterCardReq();
+      var result = client.request(charactersReq as OperationRequest).first;
+      result
+          .whenComplete(() => Success(data: result));
+      result
+          .onError((error, stackTrace) => Failure(''));
     } on Exception catch (_, e) {
       return Failure(error: APIException("No internet connection", 0, ''));
     }
-  }
-
-  Future<QueryResult> performMutation(String query,
-      {required Map<String, dynamic> variables}) async {
-    MutationOptions options =
-        MutationOptions(document: gql(query), variables: variables);
-
-    final result = await _client.mutate(options);
-
-    log(result.toString());
-
-    return result;
   }
 }
